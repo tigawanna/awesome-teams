5309c6287ce8cefccbfc358b1ebd90ed1d1f2878
tigawanna <denniskinuthiaw@gmail.com>
2023-02-28 08:27:07 +0300

    tailwind + vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+    server: {
+      host: true,
+    },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/tests/testSetup.ts"],
+  },
+});
