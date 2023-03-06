2842f3408777d21670d65442df3f59eef14db341
tigawanna <denniskinuthiaw@gmail.com>
2023-03-06 20:27:03 +0300

    added status colors

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,21 +1,38 @@
 import { TasksResponse } from "../../utils/api/tasks";
-import { TaskSatus } from "./TaskSatus";
+
 
 interface TaskCardProps {
     task: TasksResponse
 }
 
-export const TaskCard = ({task}:TaskCardProps) => {
-return (
-    <div className='h-full border p-2 rounded-lg w-[90%] md:w-[40%] gap-2 
-        flex flex-col  justify-center  '>
-    
-     <div className='h-full flex flex-col items-center justify-center line-clamp-4'>
-        <h1 className='text-xl font-bold w-full px-1'>{task.title}</h1>
-            <p className=''>{task.description}</p>
-     </div>
-<TaskSatus task={task}/>
+export const TaskCard = ({ task }: TaskCardProps) => {
+    const statusColors = {
+        "submited": "#330c4a",
+        "approved": "#FFC107",
+        "funded": "#00BCD4",
+        "in_progress": "#22fa0a",
+        "completed": "#d0aae6",
+        "cancelled": "#F44336"
+    };
 
- </div>
-);
+    return (
+        <div
+            style={{ border: `1px solid ${statusColors[task.status]}` }}
+            className='h-full p-2 rounded-lg w-[95%] md:w-[40%] gap-2 
+        flex flex-col  justify-center  '>
+
+            <div className='h-full flex flex-col items-center justify-center line-clamp-4'>
+                <h1
+                    style={{ color: `${statusColors[task.status]}` }}
+                    className='text-xl font-bold w-full px-1'>{task.title}</h1>
+                <p className='text-sm'>{task.description}</p>
+            </div>
+            <div
+                style={{ color: statusColors[task.status] }}
+                className={''}>
+                <p className=' px-1 rounded-xl font-bold font-mono '>{task.status}</p>
+            </div>
+
+        </div>
+    );
 }
