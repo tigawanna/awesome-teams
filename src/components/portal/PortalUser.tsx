aa8ca0622d640b58fdd4d8850e052cd4fb5ae04f
tigawanna <denniskinuthiaw@gmail.com>
2023-03-14 21:09:54 +0300

    protal fixes

--- src/components/portal/PortalUser.tsx
+++ src/components/portal/PortalUser.tsx
@@ -11,20 +11,23 @@ return <div className="h-full flex items-center justify-center">No User</div>
  }   
 return (
  <div className='w-full h-full flex items-center justify-center'>
- <div className="w-full flex items-center justify-center">
+ <div className="w-fit p-2 flex flex-col items-center justify-center border rounded-xl gap-1 ">
             <img
                 src={makeImageUrl('staff', user?.id as string, user.avatar)}
                 alt="staff avatar"
                 height={"100"}
                 width={"auto"}
-                className="max-h-[200px] rounded-lg aspect-square"
+                className="max-h-[300px] rounded-lg aspect-square"
             />
-            <div className="h-full flex flex-col  justify-start">
+            <div className="w-full flex flex-col  justify-start items-start ">
                 <h1 className="text-xl font-bold ">{user.name}</h1>
                 <h1 className="text-lg font-serif">{user.email}</h1>
-                <h1 className="w-fit p-1 border rounded-lg">{user.type}</h1>
+                <h1 className="border rounded-lg">{user.type}</h1>
 
             </div>
+ </div>
+ <div>
+    
  </div>
  </div>
 );
--- src/pages/portal/Portal.tsx
+++ src/pages/portal/Portal.tsx
@@ -1,13 +1,14 @@
+import { PortalUser } from "../../components/portal/PortalUser";
 import { AppUser } from "../../utils/types/base";
 
 interface PortalProps {
 user:AppUser
 }
 
-export default function Portal({}:PortalProps){
+export default function Portal({user}:PortalProps){
   return (
-    <div className='w-full h-full'>
-      Portal component
+    <div className='w-full h-full min-h-screen  flex items-center justify-center '>
+   <PortalUser user={user}/>
     </div>
   );
 };
