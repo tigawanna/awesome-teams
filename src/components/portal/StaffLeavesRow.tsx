594c1491c68cd82cc02f160fecee430be5c992ab
tigawanna <denniskinuthiaw@gmail.com>
2023-03-22 19:08:43 +0300

    color coding leave status

--- src/components/portal/StaffLeavesRow.tsx
+++ src/components/portal/StaffLeavesRow.tsx
@@ -36,7 +36,18 @@ const mutation = useMutation({
         setError({ name: "main", message: concatErrors(error) });
     }
 })
+function leaveStatusColors(status:StaffLeaveResponse['leave_request_status']){
+if(status==="pending"){
+return "blue"
+}
+else if(status==="approved"){
+return "green"
+}
+else if(status==="rejected"){
+return "red"
+}
 
+}
 return (
  <div 
   className = {
@@ -50,39 +61,50 @@ return (
  >
     
     <div className='w-full flex flex-wrap items-start justify-start gap-2 m-2'>
-        <div className="w-[45%] h-full border shadow-lg flex items-center justify-center gap-1 p-1 rounded-lg">
+        
+            <div className="w-[45%] h-full border shadow-lg flex items-center justify-center gap-1 p-1 rounded-lg">
                 <h3 className="text-sm font-bold">Type:</h3>
                 <h3 className="">{leave.leave_type}</h3>
-        </div>
+            </div>
 
-    <div className="w-[50%] h-full  border shadow-lg flex items-center justify-center gap-1 p-1 rounded-lg">
-        <h3 className="text-sm font-bold">By:</h3>
-    <h3 className="">{leave.expand.leave_requested_by.name}</h3>
-    </div>
-    
-    <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
-    <h3 className="text-xs md:text-sm font-bold">Created:</h3>
-    <h1 className='text-xs'>{dayjs(leave.created).format('dddd DD-MMM-YYYY')}</h1>
-    <h1 className='text-xs'>{dayjs().to(dayjs(leave.created))}</h1>
-    </div>
+            <div className="w-[50%] h-full  border shadow-lg flex items-center justify-center gap-1 p-1 rounded-lg">
+                <h3 className="text-sm font-bold">By:</h3>
+                <h3 className="">{leave.expand.leave_requested_by.name}</h3>
+            </div>
 
-        <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
+            <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
+                <h3 className="text-xs md:text-sm font-bold">Created:</h3>
+                <h1 className='text-xs'>{dayjs(leave.created).format('dddd DD-MMM-YYYY')}</h1>
+                <h1 className='text-xs'>{dayjs().to(dayjs(leave.created))}</h1>
+            </div>
+
+            <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
                 <h3 className="text-xs md:text-sm font-bold">From:</h3>
                 <h1 className='text-xs'>{dayjs(leave.leave_start).format('dddd DD-MMM-YYYY')}</h1>
                 <h1 className='text-xs'>{dayjs().to(dayjs(leave.leave_start))}</h1>
-         </div>
-        <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
+            </div>
+
+            <div className="border flex items-center justify-center gap-1 p-1 rounded-lg">
                 <h3 className="text-xs md:text-sm font-bold">To:</h3>
                 <h1 className='text-xs'>{dayjs(leave.leave_end).format('dddd DD-MMM-YYYY')}</h1>
                 <h1 className='text-xs'>{dayjs().to(dayjs(leave.leave_end))}</h1>
-         </div>
+            </div>
+
+            <div 
+            style={{
+            background:leaveStatusColors(leave.leave_request_status)
+            }}
+            className="w-[45%] h-full border shadow-lg flex items-center justify-center gap-1 p-1 rounded-lg">
+                <h3 className="text-sm font-bold">Status:</h3>
+                <h3 className="">{leave.leave_request_status}</h3>
+            </div>
 
          <div className="w-full  dark:text-black flex flex-col items-center justify-center gap-1 p-1 rounded-lg">
           <ReactCalender 
             minDate={new Date(leave.leave_start)}
             maxDate={new Date(leave.leave_end)}
             />
-            {user?.type==="manager"&&
+            {user?.type==="manager"&& leave.leave_request_status==="pending" &&
                     <button
                         onClick={() => setOpen(true)}
                         className="w-fit px-5 py-2 bg-accent rounded-lg text-lg">
